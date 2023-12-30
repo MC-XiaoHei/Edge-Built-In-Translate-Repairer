7a478b316cf7bcf985d57821bc3ae26fe26e60a7
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-12-30 16:22:10 +0800

    feat: finish repair

--- src-bex/my-content-script.ts
+++ src-bex/my-content-script.ts
@@ -1,20 +1,20 @@
 // Hooks added here have a bridge allowing communication between the BEX Content Script and the Quasar Application.
 // More info: https://quasar.dev/quasar-cli/developing-browser-extensions/content-hooks
 
-import { bexContent } from 'quasar/wrappers'
+import {bexContent} from 'quasar/wrappers';
 
-export default bexContent((/* bridge */) => {
-  // Hook into the bridge to listen for events sent from the client BEX.
-  /*
-  bridge.on('some.event', event => {
-    if (event.data.yourProp) {
-      // Access a DOM element from here.
-      // Document in this instance is the underlying website the contentScript runs on
-      const el = document.getElementById('some-id')
-      if (el) {
-        el.value = 'Quasar Rocks!'
-      }
-    }
-  })
-  */
-})
+export default bexContent(() => {
+    window.addEventListener('load', () => {
+        console.log('Edge Built-in Translate Repairer 已加载');
+        document.querySelectorAll('code').forEach((element) => {
+            if(element.parentElement?.tagName === 'SPAN') return;
+            const parent = element.parentNode;
+            if (parent === null) return;
+            const span = document.createElement('span');
+            while (parent.firstChild) {
+                span.appendChild(parent.firstChild);
+            }
+            parent.appendChild(span);
+        });
+    });
+});
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,18 +1,13 @@
-import { RouteRecordRaw } from 'vue-router';
+import {RouteRecordRaw} from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
-  },
+    children: [
+      {path: '/popup', component: () => import('pages/PopupPage.vue')}
+    ],
+  }
 ];
 
 export default routes;
